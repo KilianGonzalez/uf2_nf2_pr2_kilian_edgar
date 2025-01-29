159097f052f80f26cacd2dd1ff446947dae6b490
Kilian <kiliangonzalez@ceroca.cat>
2025-01-29 19:31:26 +0100

    Inicio proyecto

---
+++ src/main/java/com/example/uf2_nf2_pr2_kilian_edgar/Uf2Nf2Pr2KilianEdgarApplication.java
@@ -0,0 +1,13 @@
+package com.example.uf2_nf2_pr2_kilian_edgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uf2Nf2Pr2KilianEdgarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uf2Nf2Pr2KilianEdgarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uf2_nf2_pr2_kilian_edgar/Uf2Nf2Pr2KilianEdgarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uf2_nf2_pr2_kilian_edgar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uf2Nf2Pr2KilianEdgarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
